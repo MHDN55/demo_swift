a82f4ca25ff295534f653263199b8d7fc2677840
Mohammad Nashawi <mohammad.nashawi.55@gmail.com>
2025-08-16 22:21:35 -0700

    Initial Commit

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by Noname on 8/16/25.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
